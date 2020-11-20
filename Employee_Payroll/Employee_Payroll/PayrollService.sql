e3b8c60602fd0ab5c3dfae9071a1c2651a62d4d1
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-11-20 20:14:07 +0530

    [Prudhvi] add . creating table

--- Employee_Payroll/Employee_Payroll/PayrollService.sql
+++ Employee_Payroll/Employee_Payroll/PayrollService.sql
@@ -3,3 +3,13 @@
 */
 create database PayrollService;
 use PayrollService;
+/*UC-2 
+  Creating table 
+*/
+create table employee_payroll
+(
+id int identity(1,1) not null,
+name varchar(25) not null,
+salary money not null,
+start_date date not null
+)
